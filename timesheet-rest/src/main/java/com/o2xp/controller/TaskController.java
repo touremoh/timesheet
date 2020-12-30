132e434036516565e60cf9c863dd5d306294f576
Mohamed Toure <demahom.toure@gmail.com>
2020-12-30 14:38:06 +0100

    optimze import

--- timesheet-rest/src/main/java/com/o2xp/controller/TaskController.java
+++ timesheet-rest/src/main/java/com/o2xp/controller/TaskController.java
@@ -1,20 +1,22 @@
 package com.o2xp.controller;
 
-import com.o2xp.model.Customer;
 import com.o2xp.model.Task;
 import com.o2xp.service.TaskService;
-import com.o2xp.service.TimesheetService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
