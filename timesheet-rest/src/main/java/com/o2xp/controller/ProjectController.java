449463f5386a54c4a3a46d78f94f5c10457423f0
Mohamed Toure <demahom.toure@gmail.com>
2020-12-31 17:46:21 +0100

    find all projects

--- timesheet-rest/src/main/java/com/o2xp/controller/ProjectController.java
+++ timesheet-rest/src/main/java/com/o2xp/controller/ProjectController.java
@@ -14,10 +14,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -37,6 +38,24 @@ public class ProjectController {
     public ResponseEntity<ProjectDTO> addProject(@RequestBody ProjectDTO projectDTO) {
         Project savedProject = this.service.save(ProjectMapper.INSTANCE.toModel(projectDTO));
         return new ResponseEntity<>(ProjectMapper.INSTANCE.toDTO(savedProject), HttpStatus.CREATED);
+    }
+    @Operation(summary = "Project finding service", description = "Finding all projects")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Returns all projects",
+            content = { @Content(schema = @Schema(anyOf = { Project.class })) }
+    )
+    @ApiResponse(
+            responseCode = "404",
+            description = "No project was found",
+            content = { @Content(schema = @Schema(hidden = true)) }
+    )
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ProjectDTO>> findAllProjects() {
+        List<Project> projects = this.service.findAll();
+        List<ProjectDTO> projectDTOS = new ArrayList<>();
+        projects.forEach(project -> projectDTOS.add(ProjectMapper.INSTANCE.toDTO(project)));
+        return new ResponseEntity<>(projectDTOS, HttpStatus.OK);
     }
 
 }
